b1d8dd710f5fa20debc7f76beeb711508eb79ed6
Chinemerem Daniel <danpresh302@gmail.com>
2024-05-07 01:41:24 +0100

    finished the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,16 @@
-import { Box, Flex, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  ListItem,
+  SimpleGrid,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
 import runner from "../assets/Running man.svg";
+
 interface InfoCardProps {
   title: string;
   value: number;
@@ -40,40 +51,48 @@ const About = () => {
           />
         </Box>
         <Flex
-          className="bounce"
+          className="bounce purple-glass"
           padding="1rem 1.5rem"
-          bgColor="purple"
-          fontSize="1.2rem"
           borderRadius="1rem"
-          textAlign="justify"
-          alignItems="stretch">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ratione
-          magnam alias quis debitis nemo numquam unde pariatur. Ipsa deserunt at
-          eius quas voluptates quae? Soluta veniam maxime nam corrupti? Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ab ratione magnam
-          alias quis debitis nemo numquam unde pariatur. Ipsa deserunt at eius
-          quas voluptates quae? Soluta veniam maxime nam corrupti? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ab ratione magnam alias
-          quis debitis nemo numquam unde pariatur. Ipsa deserunt at eius quas
-          voluptates quae? Soluta veniam maxime nam corrupti?
+          flexDirection="column">
+          <Heading textAlign="center" marginBottom="1rem">
+            Talents Studio
+          </Heading>
+          <Text fontSize="1.3rem" marginBottom=".7rem" fontWeight={500}>
+            Do you ?
+          </Text>
+          <UnorderedList fontWeight={300} fontSize="1.2rem" spacing=".5rem">
+            <ListItem> Have a project idea in need of Devs?</ListItem>
+            <ListItem> Need maintenance on am existing tech project?</ListItem>
+            <ListItem>Require consultation ?</ListItem>
+          </UnorderedList>
+          <Text fontWeight={400} marginTop="1rem">
+            Whether you need a full team or a single techie, Naledi Studio gives
+            you access to all the talent you need for your project. Let us know
+            what you want to build <Link textDecoration="underline">here</Link>
+          </Text>
         </Flex>
         <Flex
-          className="bounce"
+          className="bounce purple-glass"
           padding="1rem 1.5rem"
-          bgColor="purple"
-          fontSize="1.2rem"
           borderRadius="1rem"
-          textAlign="justify"
-          alignItems="stretch">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ratione
-          magnam alias quis debitis nemo numquam unde pariatur. Ipsa deserunt at
-          eius quas voluptates quae? Soluta veniam maxime nam corrupti? Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ab ratione magnam
-          alias quis debitis nemo numquam unde pariatur. Ipsa deserunt at eius
-          quas voluptates quae? Soluta veniam maxime nam corrupti? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ab ratione magnam alias
-          quis debitis nemo numquam unde pariatur. Ipsa deserunt at eius quas
-          voluptates quae? Soluta veniam maxime nam corrupti?
+          flexDirection="column">
+          <Heading textAlign="center" marginBottom="1rem">
+            Talents Hub
+          </Heading>
+          <Text
+            fontWeight={400}
+            fontSize="1.2rem"
+            textAlign="justify"
+            marginTop=".5rem">
+            Learn in-demand tech skills that are relevant worldwide.
+            <br /> With our well curated course contents and skilled
+            facilitators, your tech career is just one step away.
+          </Text>
+          <Text marginY="1rem">
+            Find the right course for you{" "}
+            <Link textDecoration="underline">here</Link>{" "}
+          </Text>
         </Flex>
         <Box>
           {" "}
@@ -104,22 +123,29 @@ const About = () => {
         />
         <Flex flexDir="column" gap=".5rem" className="bounce">
           <Text fontSize="2rem" fontWeight="600">
-            With us,
+            About Us ,
           </Text>
           <Text fontSize="3rem" fontWeight="500">
-            Finding a career path promises to be easy
+            TalentTech is the home of all things tech
+          </Text>
+          <Text fontSize="1.8rem" fontWeight="500">
+            {" "}
+            TalentsTech comprises of Talents Studio and Talents Hub
           </Text>
           <Text fontSize="1rem" fontWeight="400">
-            In most beginner stories, you find that one thing is common, the
-            moment of confusion. Here, because we care about your progress, we
-            have provided very streamlined courses with a platform that promises
-            ease of use.
+            Talents Studio makes the talent available to bring our clients'
+            project ideas to reality.
+          </Text>
+          <Text fontSize="1rem" fontWeight="400">
+            Talents Hub makes it possible for our students to learn emerging
+            tech skills to jumpstart their careers. Our graduates have an
+            employment rate of 92%
           </Text>
           <SimpleGrid columns={2} spacing={25} paddingX="3rem">
-            <InfoCard title="Competent facilitators" value={200} />
-            <InfoCard title="Professional videos" value={100} />
-            <InfoCard title="Students" value={10} />
-            <InfoCard title="Sponsors" value={20} />
+            <InfoCard title="Completed Projects" value={200} />
+            <InfoCard title="Satisfied Clients" value={100} />
+            <InfoCard title="Enrolled Students" value={10} />
+            <InfoCard title="Skilled Facilitators" value={20} />
           </SimpleGrid>
         </Flex>
       </SimpleGrid>
